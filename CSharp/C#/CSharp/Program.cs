fb3222cb77bb1b28a54664b75867ed66a729e5d0
parvinkhm <[parvinkhm]>
2023-07-13 00:22:31 +0400

    added new folder

---
+++ CSharp/C#/CSharp/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+
+//Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın.
+//int n = 21;
+
+
+//if (n % 3 == 0 && n % 7 == 0)
+//{
+//    Console.WriteLine("Bolunur");
+
+//}
+//else Console.WriteLine("Bolunmur");
+
+// n və m ədədləri verilir. Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın.
+//int n = 6;
+
+//int m = 8;
+
+//int sum;
+
+//if (n % 2 == 0 && m % 2 == 0)
+//    Console.WriteLine(sum = n+m);
+
+//Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
+//int n = 6;
+
+//int m = 8;
+
+//int sum = 0;
+
+//for (int i = n; i <= m; i++)
+//{
+//    if (i % 2 == 1) ;
+//    {
+//        sum+=i;
+//        Console.WriteLine(sum);
+//    }
+
+//Verilmish arrayin icindeki tek ededlerin cemini tapin.
+//int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//int sum = 0;
+//{
+//    foreach (var item in numbers)
+
+//        if (item % 2 == 1)
+//        {
+//            sum += item;
+//        }
+//}
+//  Console.WriteLine(sum);
+
+
+//Verilmish arrayin icindeki cut ededlerin sayini tapin.
+//int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//int sum = 0;
+//{
+//    foreach (var item in numbers)
+
+//        if (item % 2 == 0)
+//        {
+//            sum += item;
+//        }
+//}
+//Console.WriteLine(sum);
+
+
+
+
+
+
+
+
+
+
+
